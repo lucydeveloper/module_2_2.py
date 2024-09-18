1de8c7461c830abe9f58425a03a5becd9fe1725c
lucydeveloper <ludmilla.girina@gmail.com>
2024-09-18 20:19:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# module_2_2.py
+first = int(input("Введите числ: "))
+second = int(input("Введите числ: "))
+third = int(input("Введите числ: "))
+if first == second and third: # если все 3 числа верны между собой
+    print(3)
+elif first == second or first == third or second == third: # если 2 числа из 3 верны между собой
+    print(2)
+else: # если нет равных между мобой нет чисел
+    print(0)
+first = int(input("Введите числ: "))
+second = int(input("Введите числ: "))
+third = int(input("Введите числ: "))
+if first == second and third: # если все 3 числа верны между собой
+    print(3)
+elif first == second or first == third or second == third: # если 2 числа из 3 верны между собой
+    print(2)
+else: # если нет равных между мобой нет чисел
+    print(0)
